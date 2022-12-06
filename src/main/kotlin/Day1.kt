0f35934ffd6357a6d3fc5c12bfcdf1890519089b
Matthew Ma <mtthman@amazon.com>
2022-12-05 17:13:36 -0800

    Solve Day 1 concurrently

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,10 +1,30 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import java.io.File
 
 class Day1 {
     fun main() {
+        measureDuration("sequential") {
+            solveWith { chunks ->
+                chunks.map { it.parseCalories() }.sortedByDescending { it }
+            }
+        }
+        measureDuration("concurrent") {
+            solveWith { chunks ->
+                runBlocking {
+                    chunks.map {
+                        async { it.parseCalories() }
+                    }.awaitAll().sortedByDescending { it }
+                }
+            }
+        }
+    }
+
+    private fun solveWith(block: (List<String>) -> List<Int>) {
         val input = File("/Users/mtthman/Desktop/AdventOfCode/src/main/kotlin/Day1.txt").readText()
         val chunks = input.parseChunks()
-        val caloriesList = chunks.map { it.parseCalories() }.sortedByDescending { it }
+        val caloriesList = block(chunks)
 
         val topElf = caloriesList.take(1).sum()
         val topThreeElves = caloriesList.take(3).sum()
